dc4f4f6bfdebae3d04ca6e787f1c6feaa73028fc
AniLahiri <128821280+AniLahiri@users.noreply.github.com>
2023-04-18 14:55:08 +0530

    index file modified

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,14 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { PersonResolver } from './PersonResolver';
 
-export default async function bootstrap() {
-  const schema = await buildSchema({
-    resolvers: [PersonResolver],
-  });
+const schema = await buildSchema({
+  resolvers: [PersonResolver],
+});
 
-  const server = new ApolloServer({ schema: schema });
+const server = new ApolloServer({ schema: schema });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-  console.log(`Server is running, GraphQL Playground available at ${url}`);
-}
+console.log(`Server is running, GraphQL Playground available at ${url}`);
